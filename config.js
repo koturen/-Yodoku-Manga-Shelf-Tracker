063c6323799c8b42b6819ff21826aec3f6b0ac0d
koturen <quoctruongkien.110909@gmail.com>
2026-07-08 19:04:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 const CONFIG = {
-  APPS_SCRIPT_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwRaamVIoDWHciE1AF_6StIPiFzZETDy1tsj3ZBws-C-_OA2WMNj9xLPv8sG3p8ms5S/exec",
 
   // Logo: để trống LOGO_IMAGE_URL để dùng logo chữ (LOGO_TEXT).
   // Có link ảnh thì sẽ ưu tiên dùng ảnh làm logo.
